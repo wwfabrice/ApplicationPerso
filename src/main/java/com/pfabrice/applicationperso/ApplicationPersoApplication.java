962a87901d1cd9271d62fd747671369f33fa3c7f
Fabrice <wwfabrice35@gmail.com>
2019-02-26 08:30:45 +0100

    create project spring boot (web, mysql)

---
+++ src/main/java/com/pfabrice/applicationperso/ApplicationPersoApplication.java
@@ -0,0 +1,13 @@
+package com.pfabrice.applicationperso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPersoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationPersoApplication.class, args);
+	}
+
+}
